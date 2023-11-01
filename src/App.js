7cb2a5ca82d7b0e0d19b43f2880bde5de5d2c872
Jonathas Borges <jbc@icomp.ufam.edu.br>
2023-11-01 00:49:41 -0400

    deploy [v0.0.3]

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React no site da Ju =)
+          Learn React no site da Ju =).
         </a>
 
       </header>

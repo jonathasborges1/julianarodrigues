95d846c51002c988c2162a840e7067404aad4309
Jonathas Borges <jbc@icomp.ufam.edu.br>
2023-12-08 01:13:58 -0400

    refactor: try optimize bundle in vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -2,18 +2,39 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 // import svgr from "vite-plugin-svgr";
 import svgr from '@svgr/rollup';
+// import commonjs from '@rollup/plugin-commonjs';
 
 const port = parseInt(process.env.PORT ?? '5000', 10);
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build:{
-    outDir: 'build', // Altere para o nome do diretório desejado de 'dist' para 'build'
-  },
   plugins: [
     react(),
     svgr(),
+    // commonjs(),
   ],
+  build:{
+    sourcemap: false,
+    outDir: 'build', // Altere para o nome do diretório desejado de 'dist' para 'build'
+    modulePreload: {
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      resolveDependencies: (url, deps, context) => {
+        return [];
+      }
+    },
+    rollupOptions: {
+      output: {
+        sourcemap: false,
+        manualChunks: {
+          ethers: ['ethers'],
+          router: ['react-router-dom'],
+          rtk: ['@reduxjs/toolkit'],
+          redux: ['react-redux'],
+          chakra: ['@chakra-ui/react'],
+        },
+      },
+    },
+  },
   server: {
     // hmr: {
     //   host: '0.0.0.0',
@@ -21,5 +42,7 @@ export default defineConfig({
     // },
     host: '0.0.0.0',
     port,
+    open: true,
+    cors: true
   },
 });

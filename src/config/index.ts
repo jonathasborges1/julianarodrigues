22d689b9d185bb730eb5db8bfebfedd70356304b
Jonathas Borges <jbc@icomp.ufam.edu.br>
2024-01-13 19:30:39 -0400

    fix: siteKey in recaptcha

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,8 +1,8 @@
 const ConfigApp = {
-   RECAPTCHA_KEY: import.meta.env.VITE_RECAPTCHA_SITE_KEY,
-   CLIENT_SECRET: import.meta.env.VITE_CLIENTE_SECRET,
-   REFRESH_TOKEN: import.meta.env.VITE_REFRESH_TOKEN,
-   REFRESH_TOKEN_DO_JHON: import.meta.env.VITE_REFRESH_TOKEN_DO_JHON,
+   RECAPTCHA_KEY: import.meta.env.VITE_RECAPTCHA_SITE_KEY ?? "6Lda30cpAAAAAHBjRWapooRLO72rh83_VbbKevnf",
+   CLIENT_SECRET: import.meta.env.VITE_CLIENTE_SECRET ?? "",
+   REFRESH_TOKEN: import.meta.env.VITE_REFRESH_TOKEN ?? "",
+   REFRESH_TOKEN_DO_JHON: import.meta.env.VITE_REFRESH_TOKEN_DO_JHON ?? "",
 }
 
 export default ConfigApp
